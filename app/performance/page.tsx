1355cbdf23c29d0ccf941bc1236e6939e873eb5f
faiz <faiz.firdaus.2004@gmail.com>
2023-08-16 23:09:16 +0400

    i

--- app/performance/page.tsx
+++ app/performance/page.tsx
@@ -1,16 +1,15 @@
-"use client"
+
 
 import ListExercises from "@/components/ListExercises";
 import { ExerciseType } from "@/types";
 import { HiArrowLeft } from "react-icons/hi";
 import React from "react";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
 
 async function getExercises() {
 
 
-  const res = await fetch(`http://localhost:3000/api/exercise?categoryId=11`, {
+  const res = await fetch(`${process.env.BASE_URL}/api/exercise?categoryId=11`, {
     cache: "no-store",
   });
 
@@ -22,17 +21,18 @@ async function getExercises() {
 
 export default async function Performance() {
 
-  const router = useRouter();
   const exercise: ExerciseType[] = await getExercises();
   console.log(exercise);
 
   return (
     <div className="justify-center flex">
       <div className="mt-24">
-        <div onClick={() => router.push("/")} className="group flex gap-x-1 items-center capitalize hover:text-emerald-600">
+        <Link href={"/"}>
+        <div className="group flex gap-x-1 items-center capitalize hover:text-emerald-600">
           <HiArrowLeft className="group-hover:-translate-x-1 transition" />
           back to categories
         </div>
+        </Link>
         <div className="capitalize text-2xl font-bold">
           Enhance performance workout plans
         </div>
--- app/weightloss/page.tsx
+++ app/weightloss/page.tsx
@@ -1,15 +1,11 @@
-"use client"
-
 import ListExercises from "@/components/ListExercises";
 import { ExerciseType } from "@/types";
 import { HiArrowLeft } from "react-icons/hi";
 import React from "react";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 async function getExercises() {
-
-
-  const res = await fetch(`http://localhost:3000/api/exercise?categoryId=9`, {
+  const res = await fetch(`${process.env.BASE_URL}/api/exercise?categoryId=9`, {
     cache: "no-store",
   });
 
@@ -20,18 +16,18 @@ async function getExercises() {
 }
 
 export default async function Weightloss() {
-
-  const router = useRouter();
   const exercise: ExerciseType[] = await getExercises();
   console.log(exercise);
 
   return (
     <div className="justify-center flex">
       <div className="mt-24">
-        <div onClick={() => router.push("/")} className="group flex gap-x-1 items-center capitalize hover:text-emerald-600">
-          <HiArrowLeft className="group-hover:-translate-x-1 transition" />
-          back to categories
-        </div>
+        <Link href={"/"}>
+          <div className="group flex gap-x-1 items-center capitalize hover:text-emerald-600">
+            <HiArrowLeft className="group-hover:-translate-x-1 transition" />
+            back to categories
+          </div>
+        </Link>
         <div className="capitalize text-2xl font-bold">
           Weight loss workout plans
         </div>
