4db33eef84d1fc2434f1f32c5a726b1833aa6dd8
faiz <faiz.firdaus.2004@gmail.com>
2023-08-17 14:31:07 +0400

    i

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import Cards from "./components/Cards";
 
 async function getCategory() {
-  const res = await fetch(`${process.env.BASE_URL}/api/category`, {
+  const res = await fetch(`${process.env.VERCEL_URL}/api/category`, {
     cache: "no-store",
   });
 
